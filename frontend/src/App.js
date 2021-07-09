9a8d65d92e4269e59a3484042aa160de04b67ba9
ohhondgi <5ghdrl@naver.com>
2021-07-09 23:56:23 +0900

    build: setting front-end & UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css'
+import Main from './js/Main'
+import Result from './js/Result'
+import {Route} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route path="/" component={Main} exact/>
+      <Route path="/result" component={Result} />
     </div>
   );
 }
---
+++ frontend/src/js/Result.js
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import '../css/index.css';
+import Logo from '../css/GAGA2.png';
+import {Link} from "react-router-dom";
+import ReactPlayer from 'react-player'
+
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+`;
+
+const Box = styled.div`
+  width:100%;
+  height:50%;
+  border: 0px;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+`
+const Text = styled.p`
+  color: black;
+  font-size:2em;
+  margin-top:15%;
+  font-family: 'Do Hyeon';
+`
+const Img = styled.img`
+  width: 12%;
+  height: 24%;
+  position: absolute;
+  margin: 1% 45%;
+`
+
+const Result= () => {
+    return(
+        <Container>
+            <Link to = "/">
+            <Img src={Logo} alt = "logo" />
+            </Link>
+            <Box>
+                <Text>동영상 인물 태깅 완료! </Text>
+            </Box>
+            <Box>
+            <ReactPlayer url="https://www.youtube.com/watch?v=-RDb-kSw-Ag&list=PL-FWHmxvuJ58xikmMnl2GQ610zeU0X4tz" 
+            height='300px' controls="true"/>
+            </Box> 
+        </Container>
+    )
+}
+
+export default Result;
